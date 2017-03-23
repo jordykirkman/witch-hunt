af1aaaba16e60ef097e32320a3b4557c57e61af4
Jordy Kirkman <jordyakirkman@gmail.com>
2017-03-22 23:26:06 -0400

    fleshed out event logic and changed names to reflect client vs server

--- app.js
+++ app.js
@@ -3,21 +3,50 @@ var nid = require('nid')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
-app.use(express.static('public'))
+app.use(express.static('../witch-hunt-client/build'))
 
-lobbies: {}
+var lobbies = {}
 
 io.sockets.on('connection', function(socket) {
   console.log('a user connected')
 
-  socket.on('start-lobby', function(){
+  socket.on('create', function(ioEvent){
     var newLobbyId = nid()
     socket.join(newLobbyId)
+    lobbies[newLobbyId] = {}
+    lobbies[newLobbyId]['players'] = {}
+    lobbies[newLobbyId]['players'][ioEvent.username] = {}
+    socket.emit('created', {lobbyId: newLobbyId})
+    socket.emit('joined', {username: ioEvent.username})
   })
 
-  socket.on('join-lobby', function(joinRequest){
-    socket.join(joinRequest.lobbyId)
-    lobbies[joinRequest.lobbyId]['players'].push({'name': joinRequest.name})
+  socket.on('join', function(ioEvent){
+    socket.join(ioEvent.lobbyId)
+
+    lobbies[ioEvent.lobbyId] = {}
+    lobbies[ioEvent.lobbyId]['players'] = {}
+    lobbies[ioEvent.lobbyId]['players'][ioEvent.username] = {}
+  })
+
+  socket.on('kill', function(ioEvent){
+    lobbies[ioEvent.lobbyId]['players'][ioEvent.username]['isDead'] = true
+    socket.in(ioEvent.lobbyId).emit('death', {username: ioEvent.username})
+  })
+
+  socket.on('submitVote', function(ioEvent){
+    let eventPlayer = lobbies[ioEvent.lobbyId]['players'][ioEvent.username]
+    eventPlayer['vote'] += 1
+    socket.in(ioEvent.lobbyId).emit('vote', {username: ioEvent.username})
+    let playerCount = 0
+    for(let key in lobbies[ioEvent.lobbyId]['players']){
+      if(lobbies[ioEvent.lobbyId]['players'][key]['isDead'] === false){
+        playerCount += 1
+      }
+    }
+    if(eventPlayer['vote'] >= (playerCount / 2)){
+      eventPlayer['isDead'] = true
+      socket.in(ioEvent.lobbyId).emit('death', {username: ioEvent.username})
+    }
   })
 
 })
