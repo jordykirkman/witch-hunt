5859ac5defb42a147bc63edea2e59da90dc6b9ad
Jordy Kirkman <jordyakirkman@gmail.com>
2017-03-28 23:17:23 -0600

    fixed voting logic fixed join lobby logic

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var http = require('http').Server(app)
 var io = require('socket.io')(http)
 app.use(express.static('../witch-hunt-client/build'))
 
-var lobbies = {}
+let lobbies = {}
+
+// let modifyPlayer = function()
 
 io.sockets.on('connection', function(socket) {
   console.log('a user connected')
@@ -14,39 +16,68 @@ io.sockets.on('connection', function(socket) {
     var newLobbyId = nid()
     socket.join(newLobbyId)
     lobbies[newLobbyId] = {}
+
+    // TODO condence into add player method
     lobbies[newLobbyId]['players'] = {}
     lobbies[newLobbyId]['players'][ioEvent.username] = {}
-    socket.emit('created', {lobbyId: newLobbyId})
-    socket.emit('joined', {username: ioEvent.username})
+    lobbies[newLobbyId]['players'][ioEvent.username]['username'] = ioEvent.username
+    lobbies[newLobbyId]['players'][ioEvent.username]['killVote'] = []
+    lobbies[newLobbyId]['players'][ioEvent.username]['isDead'] = false
+    io.sockets.in(newLobbyId).emit('created', {lobbyId: newLobbyId})
+    let playerArray = []
+    for(let key in lobbies[newLobbyId]['players']){
+      playerArray.push(lobbies[newLobbyId]['players'][key])
+    }
+    io.sockets.in(newLobbyId).emit('joined', {players: playerArray})
   })
 
   socket.on('join', function(ioEvent){
     socket.join(ioEvent.lobbyId)
 
-    lobbies[ioEvent.lobbyId] = {}
-    lobbies[ioEvent.lobbyId]['players'] = {}
+    // TODO condence into add player method
+    if(!lobbies[ioEvent.lobbyId]){
+      return
+    }
     lobbies[ioEvent.lobbyId]['players'][ioEvent.username] = {}
+    lobbies[ioEvent.lobbyId]['players'][ioEvent.username]['username'] = ioEvent.username
+    lobbies[ioEvent.lobbyId]['players'][ioEvent.username]['killVote'] = []
+    lobbies[ioEvent.lobbyId]['players'][ioEvent.username]['isDead'] = false
+    let playerArray = []
+    for(let key in lobbies[ioEvent.lobbyId]['players']){
+      playerArray.push(lobbies[ioEvent.lobbyId]['players'][key])
+    }
+    io.sockets.in(ioEvent.lobbyId).emit('joined', {players: playerArray})
   })
 
   socket.on('kill', function(ioEvent){
     lobbies[ioEvent.lobbyId]['players'][ioEvent.username]['isDead'] = true
-    socket.in(ioEvent.lobbyId).emit('death', {username: ioEvent.username})
+    let playerArray = []
+    for(let key in lobbies[ioEvent.lobbyId]['players']){
+      playerArray.push(lobbies[ioEvent.lobbyId]['players'][key])
+    }
+    io.sockets.in(ioEvent.lobbyId).emit('playerUpdate', {players: playerArray})
   })
 
   socket.on('submitVote', function(ioEvent){
-    let eventPlayer = lobbies[ioEvent.lobbyId]['players'][ioEvent.username]
-    eventPlayer['vote'] += 1
-    socket.in(ioEvent.lobbyId).emit('vote', {username: ioEvent.username})
+    lobbies[ioEvent.lobbyId]['players'][ioEvent.username]['killVote'].push({username: ioEvent.from})
+    // io.sockets.in(ioEvent.lobbyId).emit('vote', {username: ioEvent.username})
+    // count our alive players
     let playerCount = 0
     for(let key in lobbies[ioEvent.lobbyId]['players']){
       if(lobbies[ioEvent.lobbyId]['players'][key]['isDead'] === false){
         playerCount += 1
       }
     }
-    if(eventPlayer['vote'] >= (playerCount / 2)){
-      eventPlayer['isDead'] = true
-      socket.in(ioEvent.lobbyId).emit('death', {username: ioEvent.username})
+    // if there is a majority vote, kill the player
+    if(lobbies[ioEvent.lobbyId]['players'][ioEvent.username]['killVote'].length >= (playerCount / 2)){
+      lobbies[ioEvent.lobbyId]['players'][ioEvent.username]['isDead'] = true
     }
+    // format and return the array
+    let playerArray = []
+    for(let key in lobbies[ioEvent.lobbyId]['players']){
+      playerArray.push(lobbies[ioEvent.lobbyId]['players'][key])
+    }
+    io.sockets.in(ioEvent.lobbyId).emit('playerUpdate', {players: playerArray})
   })
 
 })
