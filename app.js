480c870d666d0f252ce49153d1590131e20f2bdc
Jordy Kirkman <jordyakirkman@gmail.com>
2017-04-23 11:02:21 -0600

    ¯\_(ツ)_/¯

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const io                = require('socket.io')(http)
 const PORT              = process.env.PORT || 3000
 
 app.enable('trust proxy')
-app.use(express.static('https://storage.cloud.google.com/witch-hunt-165321'))
+app.use(express.static('gcr.io/witch-hunt-165321'))
 
 let lobbies = {}
 
